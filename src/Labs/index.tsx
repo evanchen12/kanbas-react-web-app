c665b555ccf26b78af608677883cbc135a4ae7fb
evanchen12 <chen.ev@northeastern.edu>
2024-02-16 15:59:06 -0500

    src nav changes

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,13 +2,18 @@ import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
 import Assignment5 from "./a5";
+import {Routes, Route, Navigate} from "react-router";
 
 function Labs() {
   return (
-  <div>
+  <>
     <Nav />
-    <Assignment3 />
-  </div>
+    <Routes>
+      <Route path="a3" element={<Assignment3 />}></Route>
+      <Route path="a4" element={<Assignment4 />}></Route>
+      <Route path="a5" element={<Assignment5 />}></Route>
+    </Routes>
+  </>
   );
 }
  
